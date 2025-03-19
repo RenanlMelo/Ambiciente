26d9f5ae384e4418a44d2d8a733c0bf9e7fd9d41
RenanlMelo <renanlaramelo@gmail.com>
2025-03-19 20:21:15 -0300

    Corrigindo funcionalidade de atualizar informações do artigo

--- front_end/app/layout.tsx
+++ front_end/app/layout.tsx
@@ -4,6 +4,7 @@ import { Header } from "./components/header";
 import { Roboto } from "next/font/google";
 import { HeaderProvider } from "./contexts/HeaderContext";
 import { AdminProvider } from "./contexts/AdminContext";
+import Footer from "./components/footer";
 
 const roboto = Roboto({
   weight: ["100", "300", "400", "500", "700"],
@@ -32,6 +33,7 @@ export default function RootLayout({
             <HeaderProvider>
               <Header />
               {children}
+              {/* <Footer /> */}
             </HeaderProvider>
           </AdminProvider>
         </div>
