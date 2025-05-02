5730cc922fedbb5715c0ad3459fd67476a889e94
RenanlMelo <renanlaramelo@gmail.com>
2025-05-02 01:58:02 -0300

    Corrigindo erro no build

--- front_end/app/(private)/(admin)/artigos/criar/page.tsx
+++ front_end/app/(private)/(admin)/artigos/criar/page.tsx
@@ -1,6 +1,5 @@
 import { Create_article } from "@/app/components/api_articles/create_article";
 import PrivateRoute from "@/app/components/auth/privateRoute";
-import Footer from "@/app/components/ui/footer";
 
 export default function Create() {
   return (
--- front_end/app/(private)/(admin)/artigos/editar/[slug]/page.tsx
+++ front_end/app/(private)/(admin)/artigos/editar/[slug]/page.tsx
@@ -1,6 +1,5 @@
 import { Edit_article } from "@/app/components/api_articles/edit_article";
 import PrivateRoute from "@/app/components/auth/privateRoute";
-import Footer from "@/app/components/ui/footer";
 
 export default function Edit() {
   return (
