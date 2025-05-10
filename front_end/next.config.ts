c5ce5d9502f1f4323b49ca31512665cb3dbd4c38
RenanlMelo <renanlaramelo@gmail.com>
2025-05-10 12:26:03 -0300

    Updating public folder and metadata info

--- front_end/next.config.ts
+++ front_end/next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_API_URL_PROD: "https://ambiciente.onrender.com",
     NEXT_PUBLIC_API_URL_HOMOLOG: "http://127.0.0.1:8000",
-    // NEXT_PUBLIC_API_URL_HOMOLOG: "https://ambiciente.onrender.com",
   },
   images: {
     domains: ["ambiciente.onrender.com", "127.0.0.1"],
